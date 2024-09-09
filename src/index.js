13b949e095a85e37111203fe5831248019c51e67
hikerpig <hikerpigwinnie@gmail.com>
2024-09-09 20:12:18 +0800

    init for issue bug reproduction

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+import zip from 'lodash.tar'
+
+console.log('zip fn is', zip)
+
+document.querySelector('#root').innerHTML = `
+<div class="content">
+  <h1>Vanilla Rspack</h1>
+  <p>Start building amazing things with Rspack.</p>
+</div>
+`
